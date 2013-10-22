3220a594b34567a8f448944aa53567b4c0824d73
John Macdonald <jnmacdnld@gmail.com>
2013-10-22 12:28:16 -0400

    use ArmControl for arm macros and remove manual arm movement

--- arm.c
+++ arm.c
@@ -1,16 +1,11 @@
 #ifndef ARM_C
 #define ARM_C
 
-
 #include "motor.c"
 
 #define ARM_UP_POS   3080
 #define ARM_DOWN_POS 1400
 #define ARM_MIDDLE_POS 1500
-
-#define ARM_UP_PWR    FULL_PWR
-#define ARM_DOWN_PWR -FULL_PWR / 3
-#define ARM_HOLD_PWR  10
 
 #define armPos         SensorValue[armPot]
 
@@ -20,40 +15,20 @@
 #define armDownMacroPressed vexRT[Btn8D]
 #define armMiddleMacroPresed vexRT[Btn8L]
 
-int armControlTarget = -1;
-int armControlPwr = 0;
-bool armControlActive = false;
-
 void armSetPowerRaw(int value);
 
 bool armIsDown();
 bool armIsUp();
 
-void armUpPressedCb();
-void armDownPressedCb();
-
-void armControlSetTarget(int target);
-void armControlStep();
-
-void holdArmPos();
-void updateArm();
-
 void updateArm() {
-	if (armUpMacroPressed && !armIsUp() )
-		armControlSetTarget(ARM_UP_POS);
-	else if (armDownMacroPressed && !armIsDown() )
-		armControlSetTarget(1310);
-	else if (armMiddleMacroPresed)
-		armControlSetTarget(ARM_MIDDLE_POS);
-
-	if (armUpPressed)
-		armUpPressedCb();
-	else if (armDownPressed)
-		armDownPressedCb();
-	else if (armControlActive)
-		armControlStep();
-	else
-		holdArmPos();
+	if (armUpMacroPressed) {
+		armControlMoveToPos(ARM_UP_POS);
+	} else if (armDownMacroPressed) {
+		armControlDisable();
+		armSetPwrRaw(0);
+	} else if (armMiddleMacroPresed) {
+		armControlMoveToPos(ARM_MIDDLE_POS);
+	}
 }
 
 void armSetPowerRaw(int value) {
@@ -67,54 +42,6 @@ bool armIsDown() {
 
 bool armIsUp() {
 	return (armPos >= ARM_UP_POS);
-}
-
-void armUpPressedCb() {
-	if ( armIsUp() ) {
-		holdArmPos();
-		return;
-	}
-
-	armSetPowerRaw(ARM_UP_PWR);
-	armControlActive = false;
-}
-
-void armDownPressedCb() {
-	if ( armIsDown() ) {
-		holdArmPos();
-		return;
-	}
-
-	armSetPowerRaw(ARM_DOWN_PWR);
-	armControlActive = false;
-}
-
-void armControlSetTarget(int target) {
-	if (armControlTarget == target)
-		return;
-
-	if (armPos < target)
-		armControlPwr = ARM_UP_PWR;
-	else
-		armControlPwr = ARM_DOWN_PWR;
-
-	armControlTarget = target;
-	armControlActive = true;
-}
-
-void armControlStep() {
-	if (armControlPwr > 0 && armPos < armControlTarget ||
-			armControlPwr < 0 && armPos > armControlTarget)
-		armSetPowerRaw(armControlPwr);
-	else
-	   armControlActive = false;
-}
-
-void holdArmPos() {
-	if ( armIsDown() ) // Don't try to hold up the arm if it's all the way down
-			armSetPowerRaw(0);
-	else
-		armSetPowerRaw(ARM_HOLD_PWR); // Hold up the arm so it doesn't fall
 }
 
 #endif
