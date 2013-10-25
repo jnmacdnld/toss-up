1697f9b1288d71cf88af9a9883886b17ed6e0093
John Macdonald <jnmacdnld@gmail.com>
2013-10-24 22:23:40 -0400

    new arcade drive code (needs testing)

--- arcade_drive.c
+++ arcade_drive.c
@@ -1,69 +1,56 @@
 #ifndef ARCADE_DRIVE_C
 #define ARCADE_DRIVE_C
 
+#define MAX_TURN 127.0
+
 #include "drive.c"
 
-typedef struct {
-	short sign;
-	short raw;
-	float mag;
-	int cmd;
-} DriveMotor;
-
-void normalizeDrive(DriveMotor* left, DriveMotor* right);
+void slowDrive(int& drive, int turn);
 
 void updateDriveArcadeDrive() {
-	DriveMotor left;
-	DriveMotor right;
+	int speed_axis = vexRT[Ch3];
+	int turn_axis = vexRT[Ch1];
 
-	short speed_axis = (short) vexRT[Ch3];
-	short turn_axis = (short) vexRT[Ch1];
-
-	if (abs(speed_axis) < 10)
+	// If the joysticks have been moved very very little, ignore it
+	if ( abs(speed_axis) < 10)
 		speed_axis = 0;
 	
-	if (abs(turn_axis) < 10 )
+	if ( abs(turn_axis) < 10 )
 		turn_axis = 0;
 
-	if (speed_axis < -50) {
-		left.raw = speed_axis - turn_axis;
-		right.raw = speed_axis + turn_axis;
-	} else {
-		left.raw = (short) (speed_axis + turn_axis);
-		right.raw = (short) (speed_axis - turn_axis);
+	// If no joysticks have been moved, do nothing
+	if (speed_axis == 0 && turn_axis == 0) {
+		setLeftDrive(0);
+		setRightDrive(0);
+
+	// If only the turn joystick has been moved, pivot
+	} else if (speed_axis == 0 && turn_axis != 0) {
+		setLeftDrive(speed_axis);
+		setRightDrive(-speed_axis);
+
+	// If only the speed joystick has been moved (no turn), move straight
+	} else if (speed_axis != 0 && turn_axis == 0) {
+		setLeftDrive(speed_axis);
+		setRightDrive(speed_axis);
+
+	// If the speed joystick and turn joystick has been moved,
+	// slow one of the wheels to turn the robot
+	} else if (speed_axis != 0 && turn_axis != 0) {
+		int left = speed_axis;
+		int right = speed_axis;
+
+		if (turn_axis > 0)
+			slowDrive(right, turn_axis);
+		else
+			slowDrive(left, turn_axis);
+
+		setLeftDrive(left);
+		setRightDrive(right);
 	}
-
-	left.mag = (float) abs(left.raw);
-	right.mag = (float) abs(right.raw);
-
-	if (left.mag > FULL_PWR || right.mag > FULL_PWR) {
-		normalizeDrive(&left, &right);
-	} else {
-		left.cmd = (int) left.raw;
-		right.cmd = (int) right.raw;
-	}
-
-	setLeftDrive(left.cmd);
-	setRightDrive(right.cmd);
 }
 
-void normalizeDrive(DriveMotor* left, DriveMotor* right) {
-	left->sign = sgn(left->raw);
-	right->sign = sgn(right->raw);
-
-	DriveMotor* greater;
-	DriveMotor* lesser;
-
-	if (left->mag > right->mag) {
-		greater = left;
-		lesser = right;
-	} else {
-		greater = right;
-		lesser = left;
-	}
-
-	greater->cmd = FULL_PWR * greater->sign;
-	lesser->cmd = (short) ( (lesser->mag / greater->mag) * FULL_PWR * lesser->sign );
+void slowDrive(int& drive, int turn) {
+	drive *= 1 - ( abs(turn) / MAX_TURN );
 }
 
 #endif
