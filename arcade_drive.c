3892f2bd0eec1f43c00943c48b7f42c190cf6ed7
John Macdonald <jnmacdnld@gmail.com>
2014-03-04 08:10:40 -0500

    make it difficult to pivot unintentionally

--- arcade_drive.c
+++ arcade_drive.c
@@ -6,24 +6,36 @@
 #include "drive.c"
 #include "joystick.c"
 
-int left, right;
+bool arcade_drive_can_pivot;
 
 void ArcadeDriveSlowDrive(int& drive, int turn);
 
 void ArcadeDriveUpdate() {
+	int left, right;
 	int speed_axis = JoystickGet(kLeftStickYAxis);
 	int turn_axis = JoystickGet(kRightStickXAxis);
 
+	// If the robot is curve turning, it can only pivot turn after the turn stick
+	// has been released
+	if (speed_axis != 0 && turn_axis != 0)
+		arcade_drive_can_pivot = false;
+
+	// Allow pivoting when the turn stick is released
+	if (turn_axis == 0)
+		arcade_drive_can_pivot = true;
+
 	// If the speed joystick has not been moved,
 	// pivot according to the turn joystick
-	if (speed_axis == 0) {
-		left = turn_axis;
+	if (speed_axis == 0 && arcade_drive_can_pivot)
+	{
+  	left = turn_axis;
 		right = -turn_axis;
 		DriveSetLeft(left);
 		DriveSetRight(right);
-
+	}
 	// If the speed joystick has been moved, curve accoring to the turn joystick
-	} else {
+	else if (speed_axis != 0)
+	{
 		left = speed_axis;
 		right = speed_axis;
 
@@ -43,7 +55,8 @@ void ArcadeDriveUpdate() {
 	}
 }
 
-void ArcadeDriveSlowDrive(int& drive, int turn) {
+void ArcadeDriveSlowDrive(int& drive, int turn)
+{
 	float drive_f = drive;
 	float turn_f = abs(turn);
 
