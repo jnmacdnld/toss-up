89d9a0f68a12734f4ec7dcf9f9bc2b2d7d839194
John Macdonald <jnmacdnld@gmail.com>
2014-03-28 11:54:59 -0400

    move debug variables to a separate module

--- UserControl.c
+++ UserControl.c
@@ -1,5 +1,5 @@
-#ifndef USER_CONTROL_C
-#define USER_CONTROL_C
+#ifndef USER_CONTROL
+#define USER_CONTROL
 
 #include "Drive.c"
 #include "Intake.c"
@@ -8,46 +8,15 @@
 #include "GyroLib.c"
 #include "TankDrive.c"
 
-int armPotVal;
-int _backLeftDriveEncoder;
-int _backRightDriveEncoder;
-float gyroValue;
-int gyroValueRaw;
-int sonarValue;
-int leftFrontLineValue;
-
-bool seeLine;
-int lineNumber;
-
-task UserControl() {
-  ArmControlReset();
-          
-  while (true) {
+task UserControl()
+{
+  while (true)
+  {
     IntakeUpdate();
     ArcadeDriveUpdate();
     ArmUpdate();
 
-    /*// Prevent tipping
-    if ( armPos > (kArmUpPos - 300) )
-      DriveSetDeltaSettingLimits(-256, 30);
-    else
-      DriveSetDeltaSettingLimits(-256, 256);*/
-
-    // For debugging
-    armPotVal = SensorValue[armPot];
-    _backLeftDriveEncoder = nMotorEncoder[backLeftDrive];
-    _backRightDriveEncoder = nMotorEncoder[backRightDrive];
-    gyroValue = GyroGetAngle();
-    gyroValueRaw = SensorValue[gyro];
-    sonarValue = SensorValue[stashSonar];
-    leftFrontLineValue = SensorValue[leftFrontLine];
-
-    seeLine = SensorValue[leftFrontLine] < 2300;
-
-    if (seeLine)
-      lineNumber++;
-
-    wait1Msec(10);
+    wait1Msec(25);
   }
 }
 
